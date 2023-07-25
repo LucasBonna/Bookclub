539d5fb3aef1ef78a9558fa20569b642d4d94282
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2023-07-25 17:16:54 -0300

    prize-draw backend

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
 const connection = pool.promise();
 
 router.get('/all', async (req: Request, res: Response) => {
-    // Query para buscar todos os usuários no banco de dados
+    // Query para buscar todos os livros no banco de dados
     const query = 'SELECT * FROM books';
 
     try {
@@ -28,7 +28,7 @@ router.get('/all', async (req: Request, res: Response) => {
 router.get('/:id', async (req: Request, res: Response) => {
     const id = parseInt(req.params.id);
 
-    // Query para buscar um usuário específico no banco de dados
+    // Query para buscar um livro específico no banco de dados
     const query = `SELECT * FROM books WHERE id = ?`;
     const values = [id];
 
@@ -52,8 +52,24 @@ router.get('/:id', async (req: Request, res: Response) => {
     }
 });
 
+router.get('/getByType/:type', async (req: Request, res: Response) => {
+    const bookType = parseInt(req.params.type);
+
+    // Query para buscar todos os livros de determinado tipo no banco de dados
+    const query = `SELECT * FROM books WHERE type = ?`;
+    const values = [bookType]
+
+    try {
+        const [results, _] = await connection.query(query, values);
+        res.status(200).json(results);
+    } catch (err) {
+        console.error('Erro ao executar consulta:', err);
+        res.status(500).json({ message: 'Erro ao obter livros.' });
+    }
+});
+
 router.post('/createProduct', async (req: Request, res: Response) => {
-    const { id, isbn, name, autor, editora, quantity, image } = req.body;
+    const { id, isbn, name, autor, editora, quantity, image, type } = req.body;
 
     try {
         // Verificar se o livro já existe no sistema
@@ -74,8 +90,8 @@ router.post('/createProduct', async (req: Request, res: Response) => {
         }
 
         // Inserir o usuário ao banco de dados
-        const insertQuery = `INSERT INTO books (isbn, name, autor, editora, quantity, image) VALUES (?, ?, ?, ?, ?, ?)`;
-        const insertValues = [isbn, name, autor, editora, quantity, image];
+        const insertQuery = `INSERT INTO books (isbn, name, autor, editora, quantity, image, type) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        const insertValues = [isbn, name, autor, editora, quantity, image, type];
 
         await connection.query(insertQuery, insertValues);
         res.status(201).send({ message: 'Produto criado com sucesso!' });
@@ -111,9 +127,64 @@ router.delete('/remove/:id', async (req: Request, res: Response) => {
     }
 });
 
+router.post('/reduceStock/:id', async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+
+    try {
+        // Verificar se o livro existe antes de alterá-lo
+        const bookExistsQuery = 'SELECT * FROM books WHERE id = ?';
+        const bookExistsValues = [id];
+        const [bookExistsResults, _] = await connection.query(bookExistsQuery, bookExistsValues);
+
+        if (Array.isArray(bookExistsResults) && bookExistsResults.length === 0) {
+            res.status(404).json({ message: 'O livro não foi encontrado.' });
+            return;
+        }
+
+        // Query de atualização de quantidade
+        const updateQuery = 'UPDATE books SET quantity = quantity - 1 WHERE id = ?';
+        const updateValues = [id];
+
+        // Executar a query de atualização
+        await connection.query(updateQuery, updateValues);
+
+        res.status(200).json({ message: 'Quantidade do livro atualizada com sucesso!' });
+    } catch (err) {
+        console.error('Erro ao executar a consulta', err);
+        res.status(500).json({ message: 'Erro ao atualizar a quantidade do livro.' });
+    }
+});
+
+router.post('/bookSelectionInsert', async (req: Request, res: Response) => {
+    const { bookId, subscriptionId, selectionDate } = req.body;
+
+    try {
+        // Verificar se o livro existe antes de alterá-lo
+        const bookExistsQuery = 'SELECT * FROM books WHERE id = ?';
+        const bookExistsValues = [bookId];
+        const [bookExistsResults, _] = await connection.query(bookExistsQuery, bookExistsValues);
+
+        if (Array.isArray(bookExistsResults) && bookExistsResults.length === 0) {
+            res.status(404).json({ message: 'O livro não foi encontrado.' });
+            return;
+        }
+
+        // Query de inserção no banco de dados
+        const Query = `INSERT INTO book_selections (book_id, subscription_id, selection_date) VALUES (?, ?, ?)`;
+        const values = [bookId, subscriptionId, selectionDate];
+        
+        await connection.query(Query, values);
+        
+        res.status(200).json({ message: 'Selection Inserida com sucesso ao banco de dados!' });
+    } catch (err) {
+        console.error('Erro ao executar a consulta.', err);
+        res.status(500).json({ message: 'Erro ao inserir dados.' });
+    }
+})
+
 router.post('/edit/:id', async (req: Request, res: Response) => {
     const id = parseInt(req.params.id);
-    const { isbn, name, autor, editora, quantity, image } = req.body;
+    const { isbn, name, autor, editora, quantity, image, type } = req.body;
 
     try {
         // Verificar se o livro existe antes de alterá-lo
@@ -154,6 +225,10 @@ router.post('/edit/:id', async (req: Request, res: Response) => {
             updateQuery += ' image = ?,';
             updateValues.push(image);
         }
+        if (type) {
+            updateQuery += ' type = ?,';
+            updateValues.push(type);
+        }
 
         // Remover a vírgula extra do final da query
         updateQuery = updateQuery.slice(0, -1);
@@ -170,5 +245,7 @@ router.post('/edit/:id', async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Erro ao atualizar o livro.' });
     }
 });
+
+
 
 export default router;
--- src/routes/subscriptions.routes.ts
+++ src/routes/subscriptions.routes.ts
@@ -52,4 +52,49 @@ router.get('/checkSubscription', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/getUserSubscription/:userId', async (req: Request, res: Response) => {
+  const userId = parseInt(req.params.userId);
+
+  try {
+    const connection = await pool.getConnection();
+    // Query para buscar uma assinatura válida do usuário no banco de dados
+    const query = `SELECT subscription_id, subscription_type FROM subscriptions WHERE user_id = ? AND status = 'active' AND end_date >= NOW() ORDER BY start_date DESC LIMIT 3`;
+    const values = [userId];
+
+    const [results, _] = await connection.query<RowDataPacket[]>(query, values);
+    connection.release();
+
+    const validSubscriptions = results.map((row) => ({
+      subscription_id: row.subscription_id,
+      subscription_type: row.subscription_type,
+    }));
+
+    res.status(200).json({ validSubscriptions });
+  } catch (err) {
+    console.error('Erro ao executar a consulta:', err);
+    res.status(500).json({ message: 'Nenhuma assinatura válida encontrada.' });
+  }
+});
+
+router.get('/checkBookSelection/:subscription_Id', async (req: Request, res: Response) => {
+  const subscription_id = parseInt(req.params.subscription_Id);
+
+  try {
+    const connection = await pool.getConnection();
+    // Query para buscar uma Book Selection daquela assinatura
+    const query = `SELECT * FROM book_selections WHERE subscription_id = ?`
+    const values = [subscription_id];
+    
+    const [results, _] = await connection.query<RowDataPacket[]>(query, values);
+    connection.release();
+
+    const bookSelected = results.length > 0 ? results[0].book_id : null;
+
+    res.status(200).json({ bookSelected });
+  } catch (err) {
+    console.error('Erro ao executar a consulta:', err);
+    res.status(500).json({ message: 'Nenhum livro foi escolhido ainda!' });
+  }
+});
+
 export default router;
