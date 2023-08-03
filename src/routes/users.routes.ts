169ea045e14a7d5088dbf72fd086681fd4999183
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2023-08-03 15:08:32 -0300

    password hash

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,39 +1,52 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
-import mysql, { Pool, OkPacket, RowDataPacket } from 'mysql2';
+import bcrypt from 'bcrypt';
+import mysql, { RowDataPacket } from 'mysql2';
 
 const router = Router();
 
 const pool = mysql.createPool({
     host: '127.0.0.1',      
-    user: 'root',    
+    user: 'root',
     password: 'Livia2005!',  
     database: 'db_clubedolivro',
 });
 
 const connection = pool.promise();
 
-router.post('/authenticate', async (req, res) => {
-    const { email, password } = req.body;
-  
-    // Query para buscar o usuário no banco de dados
-    const query = `SELECT id, phone, name, email, permission FROM users WHERE email = ? AND password = ?`;
-    const values = [email, password];
-  
-    try {
-      const [results, _] = await connection.query<RowDataPacket[]>(query, values);
-  
-      if (Array.isArray(results) && results.length > 0) {
-        const user = results[0];
-        res.status(200).json({ message: 'Autenticação de usuário completada com sucesso!', user });
+router.post('/authenticate', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // Query para buscar o usuário no banco de dados
+  const query = `SELECT id, phone, name, email, password, permission FROM users WHERE email = ?`;
+  const values = [email];
+
+  try {
+    const [results, _] = await connection.query<RowDataPacket[]>(query, values);
+
+    if (Array.isArray(results) && results.length > 0) {
+      const user = results[0];
+      const storedHashedPassword = user.password;
+
+      // Comparar a senha fornecida pelo usuário com o hash armazenado no banco de dados
+      const isPasswordCorrect = await bcrypt.compare(password, storedHashedPassword);
+
+      if (isPasswordCorrect) {
+        // Se a senha estiver correta, retornar os detalhes do usuário na resposta
+        const { password: userPassword, ...userData } = user; // Removendo a senha do objeto de resposta
+        res.status(200).json({ message: 'Autenticação de usuário completada com sucesso!', user: userData });
       } else {
         res.status(401).json({ message: 'Autenticação de usuário não concluída.' });
       }
-    } catch (err) {
-      console.error('Erro ao executar a consulta:', err);
-      res.status(500).json({ message: 'Erro ao autenticar usuário.' });
+    } else {
+      res.status(401).json({ message: 'Autenticação de usuário não concluída.' });
     }
-  });
+  } catch (err) {
+    console.error('Erro ao executar a consulta:', err);
+    res.status(500).json({ message: 'Erro ao autenticar usuário.' });
+  }
+});
+
 
 router.get('/all', async (req: Request, res: Response) => {
     // Query para buscar todos os usuários no banco de dados
@@ -96,9 +109,11 @@ router.post('/createUser', async (req: Request, res: Response) => {
             return;
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // Inserir o usuário ao banco de dados
         const insertQuery = `INSERT INTO users (name, email, password, phone, permission) VALUES (?, ?, ?, ?, ?)`;
-        const insertValues = [name, email, password, phone, permission];
+        const insertValues = [name, email, hashedPassword, phone, permission];
 
         await connection.query(insertQuery, insertValues);
         res.status(201).send({ message: 'Usuário criado com sucesso!' });
